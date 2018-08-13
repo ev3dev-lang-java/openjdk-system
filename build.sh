7f0598f3844e141d508bfe5dff7cb52bea102116
Jakub Vaněk <vanek.jakub4@seznam.cz>
2018-08-13 23:38:24 +0200

    Changes for package signing

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # https://github.com/streadway/hello-debian
-gbp buildpackage -uc -us --git-ignore-new --git-ignore-branch --git-cleaner='git clean -dfx'
+gbp buildpackage --git-ignore-new --git-ignore-branch --git-cleaner='git clean -dfx'
